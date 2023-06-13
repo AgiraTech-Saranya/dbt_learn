67652540a1f6a3fa1cc16fcd3fb8cd43ed0b8d37
Saranya Balaraman <saranya.b@agiratech.com>
2023-06-13 12:46:35 +0000

    Order Priority = Critical

---
+++ models/CusotmerOrder.sql
@@ -0,0 +1,10 @@
+with customer_orders as (
+  
+  select customer_id,customer_name, order_id, Order_Priority,Product_Name
+   from `dbt_agiratechsaranya.SampleOrders` where order_priority = 'Critical' 
+)
+
+select * from customer_orders
+
+
+
